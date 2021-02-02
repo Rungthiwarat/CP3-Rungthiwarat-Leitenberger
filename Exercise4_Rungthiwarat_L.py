03474a5c23618d301468c7708abf057bbe641868
Rungthiwarat <sunflower_ara@hotmail.com>
2021-02-02 21:04:45 +0100

    Excercise4 code

---
+++ Exercise4_Rungthiwarat_L.py
@@ -0,0 +1,10 @@
+print("---","Your Score","---")
+
+FoundationEnglish=12.5
+print("Foundation English",":",FoundationEnglish)
+GeneralBusiness=13.9
+print("General Business",":",GeneralBusiness)
+IntroductiontoComputerSystem=14.9
+print("Introduction to Computer System",":",IntroductiontoComputerSystem)
+ComputerProgramming=15.6
+print("Computer Programming",":",ComputerProgramming)
